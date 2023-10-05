3a8b19f6178120cdd3bb3bb73b16c6b40fc0d64e
Eronildo Cavalcanti <eronildoj@gmail.com>
2023-10-05 13:30:04 -0400

    Remove some unnecessary comments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'features/location/presentation/pages/location_page.dart';
 import 'injection_container.dart' as di;
-import 'my_location_app.dart';
 
 void main() async {
-  await _initApp();
-  runApp(const MyLocationApp());
-}
-
-/// Initialize the app.
-Future<void> _initApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
-  // loadLocationHistoryListAction();
+
+  runApp(const MainApp());
+}
+
+/// The main app widget.
+class MainApp extends StatelessWidget {
+  /// A [MainApp] constructor.
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LocationPage(),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('es'),
+        Locale('pt'),
+      ],
+    );
+  }
 }
